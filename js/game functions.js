2c724942dda4d56eab1930a27660ce869b2af2a0
Becca Nika <rnika3@uic.edu>
2022-11-07 17:29:47 -0600

    finished project

--- js/game functions.js
+++ js/game functions.js
@@ -36,7 +36,8 @@ function changeTriColor(value, event) {
 
     if (isNaN(value)) {
         colorpick1 = value;
-        if (keyCode == "Enter") {
+        
+        if ((colorpick1 == 'red' || colorpick1 == 'green' || colorpick1 == 'blue' || colorpick1 == 'yellow') && keyCode == 'Enter') {
             document.getElementById("next_stage").style.display = 'block';
             colors = setInterval(() => {
                 //bottom triangle
@@ -91,31 +92,31 @@ function changeTriColor(value, event) {
                     {t0.style.borderRightColor = colorSet2[3];}
                 else 
                     {t0.style.borderRightColor = colorSet1[3];}
-
-                // enables the relevant buttons
-                if (t0.style.borderBottomColor == colorSet1[0]) {
-                    document.getElementById("redbutton").disabled = false;
-                    document.getElementById("bluebutton").disabled = false;
-                    document.getElementById("yelbutton").disabled = false;
-                    document.getElementById("grebutton").disabled = false;
-                    document.getElementById("orgbutton").disabled = true;
-                    document.getElementById("tealbutton").disabled = true;
-                    document.getElementById("pinkbutton").disabled = true;
-                    document.getElementById("olivebutton").disabled = true;
-                } else if (t0.style.borderBottomColor == colorSet2[0]) {
-                    document.getElementById("orgbutton").disabled = false;
-                    document.getElementById("tealbutton").disabled = false;
-                    document.getElementById("pinkbutton").disabled = false;
-                    document.getElementById("olivebutton").disabled = false;
-                    document.getElementById("redbutton").disabled = true;
-                    document.getElementById("bluebutton").disabled = true;
-                    document.getElementById("yelbutton").disabled = true;
-                    document.getElementById("grebutton").disabled = true;
-                }
+                
                 i++;
-                if (i>value) {
+                if (i>=value) {
                     i = 0;
                     clearInterval(numbers);
+                    // enables the relevant buttons
+                    if (t0.style.borderBottomColor == colorSet1[0]) {
+                        document.getElementById("redbutton").disabled = false;
+                        document.getElementById("bluebutton").disabled = false;
+                        document.getElementById("yelbutton").disabled = false;
+                        document.getElementById("grebutton").disabled = false;
+                        document.getElementById("orgbutton").disabled = true;
+                        document.getElementById("tealbutton").disabled = true;
+                        document.getElementById("pinkbutton").disabled = true;
+                        document.getElementById("olivebutton").disabled = true;
+                    } else if (t0.style.borderBottomColor == colorSet2[0]) {
+                        document.getElementById("orgbutton").disabled = false;
+                        document.getElementById("tealbutton").disabled = false;
+                        document.getElementById("pinkbutton").disabled = false;
+                        document.getElementById("olivebutton").disabled = false;
+                        document.getElementById("redbutton").disabled = true;
+                        document.getElementById("bluebutton").disabled = true;
+                        document.getElementById("yelbutton").disabled = true;
+                        document.getElementById("grebutton").disabled = true;
+                    }
                 }
                 }, 1000);
             }
@@ -150,24 +151,152 @@ function revealButtons() {
 }
 
 function fortunes(colorpick2) {
-    switch(colorpick2) {
-        case 'blue':
 
-        case 'red':
+    // hide the buttons and instructions
+    document.getElementById("second instruction").style.display = "none"
+    document.getElementById("second input").style.display = "none"
+    document.getElementById("triangle").style.display = "none"
+    document.getElementById("redbutton").style.display = "none";
+    document.getElementById("bluebutton").style.display = "none";
+    document.getElementById("yelbutton").style.display = "none";
+    document.getElementById("grebutton").style.display = "none";
+    document.getElementById("orgbutton").style.display = "none";
+    document.getElementById("olivebutton").style.display = "none";
+    document.getElementById("tealbutton").style.display = "none";
+    document.getElementById("pinkbutton").style.display = "none";
+    //reveal the play again option
+    document.getElementById("play again").style.display = "block";
+    //reveal prompt
+    document.getElementById("fortune").style.display = "block";
+    
+    if(colorpick1 == "blue") {
+                    
+        if (colorpick1 == 'red') {
+            document.getElementById("1blue1red").style.display = "block";
 
-        case 'yellow':
+        } else if (colorpick1 == 'blue') {
+            document.getElementById("twoblue").style.display = "block";
 
-        case 'green':
+        } else if (colorpick1 == 'yellow') {
+            document.getElementById("1blue1yel").style.display = "block";
 
-        case 'orange':
+        } else if (colorpick1 == 'green') {
+            document.getElementById("1blue1gre").style.display = "block";
 
-        case 'pink':
-
-        case 'olive':
-
-        case 'teal':
-
+        }
     }
+
+    else if(colorpick1 == "red") {
+        if (colorpick1 == 'red') {
+            document.getElementById("twored").style.display = "block";
+
+        } else if (colorpick1 == 'blue') {
+            document.getElementById("1red1blue").style.display = "block"
+
+        } else if (colorpick1 == 'yellow') {
+            document.getElementById("1red1yel").style.display = "block"
+
+        } else if (colorpick1 == 'green') {
+            document.getElementById("1red1gre").style.display = "block"
+            
+        }
+    }
+
+    else if(colorpick1 == "yellow") {
+        if (colorpick1 == 'red') {
+            document.getElementById("1yel1red").style.display = "block";
+
+        } else if (colorpick1 == 'blue') {
+            document.getElementById("1yel1blue").style.display = "block";
+
+        } else if (colorpick1 == 'yellow') {
+            document.getElementById("twoyel").style.display = "block";
+
+        } else if (colorpick1 == 'green') {
+            document.getElementById("1yel1gre").style.display = "block";
+            
+        }
+    }
+
+    else if(colorpick1 == "green") {
+        if (colorpick1 == 'red') {
+            document.getElementById("1gre1red").style.display = "block";
+
+        } else if (colorpick1 == 'blue') {
+            document.getElementById("1gre1blue").style.display = "block";
+
+        } else if (colorpick1 == 'yellow') {
+            document.getElementById("1gre1yel").style.display = "block";
+
+        } else if (colorpick1 == 'green') {
+            document.getElementById("twogre").style.display = "block";
+            
+        }
+    }
+
+    else if(colorpick1 == "orange") {
+        if (colorpick1 == 'red') {
+            document.getElementById("1red1org").style.display = "block";
+
+        } else if (colorpick1 == 'blue') {
+            document.getElementById("1blue1org").style.display = "block";
+
+        } else if (colorpick1 == 'yellow') {
+            document.getElementById("1yel1org").style.display = "block";
+
+        } else if (colorpick1 == 'green') {
+            document.getElementById("1gre1org").style.display = "block";
+            
+        }
+    }
+
+    else if(colorpick1 == "pink") {
+        if (colorpick1 == 'red') {
+            document.getElementById("1red1pink").style.display = "block";
+
+        } else if (colorpick1 == 'blue') {
+            document.getElementById("1blue1pink").style.display = "block";
+
+        } else if (colorpick1 == 'yellow') {
+            document.getElementById("1yel1pink").style.display = "block";
+
+        } else if (colorpick1 == 'green') {
+            document.getElementById("1gre1pink").style.display = "block";
+            
+        }
+    }
+
+    else if(colorpick1 == "olive") {
+        if (colorpick1 == 'red') {
+            document.getElementById("1red1oli").style.display = "block";
+
+        } else if (colorpick1 == 'blue') {
+            document.getElementById("1blue1oli").style.display = "block";
+
+        } else if (colorpick1 == 'yellow') {
+            document.getElementById("1yel1oli").style.display = "block";
+
+        } else if (colorpick1 == 'green') {
+            document.getElementById("1gre1oli").style.display = "block";
+            
+        }
+    }
+
+    else if(colorpick1 == "teal") {
+        if (colorpick1 == 'red') {
+            document.getElementById("1red1t").style.display = "block";
+
+        } else if (colorpick1 == 'blue') {
+            document.getElementById("1blue1t").style.display = "block";
+
+        } else if (colorpick1 == 'yellow') {
+            document.getElementById("1yel1t").style.display = "block";
+
+        } else if (colorpick1 == 'green') {
+            document.getElementById("1gre1t").style.display = "block";
+            
+        }
+    }   
     
 }
 
